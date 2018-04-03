269ba2d4c5cd696f072e529964d3ddb22d13e960
Nicholas Hoover <n.hoover13@gmail.com>
2018-04-02 21:38:47 -0700

    work

--- NicksWebsite/settings.py
+++ NicksWebsite/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-from settings_secret import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from NicksWebsite import settings_secret
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'NicksWebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql',
+        'USER': 'root',
+        'PASSWORD': settings_secret.DB,
+        'host': 'localhost',
     }
 }
 
